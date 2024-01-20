b9d8d6d3408fce50eb3f432f52375f12400a3518
ShimJiHoon <wlgns2223@gmail.com>
2024-01-21 01:12:24 +0900

    Initial Commit

---
+++ ds/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ds
+//
+//  Created by 심지훈 on 1/21/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
